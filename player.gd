73673b568c66a2fa19c2719a2ba4243866bf6d3f
Zodiax12 <olewu@wp.pl>
2026-03-17 19:07:26 +0100

    transformacja
    transformacja szczura

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends Area2D
 @export var speed = 200 
 var screen_size
+var magik = load("res://grafiki/szcur_transform.png")
 func _ready():
 	screen_size = get_viewport_rect().size
 
@@ -15,6 +16,13 @@ func _process(delta):
 		velocity.y +=1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -=1
+	
+	if Input.is_action_pressed("transform"):
+		$Sprite2D.texture = magik
+		$AnimatedSprite2D.hide()
+	if velocity.x != 0:
+		$Sprite2D.flip_v = false
+		$Sprite2D.flip_h = velocity.x > 0
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
